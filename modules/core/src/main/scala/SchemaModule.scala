259ef5aa3bf9afa18dde5e50c1b8892668a372de
Graf_Blutwurst <dominicegger@bluewin.ch>
2019-03-21 13:00:50 +0100

    stuff

--- modules/core/src/main/scala/SchemaModule.scala
+++ modules/core/src/main/scala/SchemaModule.scala
@@ -132,6 +132,13 @@ final case class IsoSchemaF[F[_], A0, A, Prim[_], SumTermId, ProductTermId](
     IsoSchemaF(nt(base), iso)
 }
 
+final case class ConstSchemaF[F[_], A, Prim[_], SumTermId, ProductTermId](
+  base:F[A],
+  value:A
+) extends SchemaF[Prim, SumTermId, ProductTermId, F, A] {
+  def hmap[G[_]](nt: F ~> G): SchemaF[Prim, SumTermId, ProductTermId, G, A] = ConstSchemaF(nt(base), value)
+}
+
 final case class SelfReference[F[_], H[_], A, Prim[_], SumTermId, ProductTermId](
   private val ref: () => F[A],
   private val nattrans: F ~> H
--- modules/core/src/main/scala/migrations.scala
+++ modules/core/src/main/scala/migrations.scala
@@ -6,25 +6,43 @@ import recursion._
 
 import SchemaF._
 
-  import monocle.Iso
+import monocle.Iso
 
 sealed trait MigrationStep[Prim[_], SumTermId, ProductTermId] {
 
-  private def addField[A, B](base: Schema[B], default: A) = Iso[B, (A, B)](b => (default, b))(_._2)
+  //private def addField[A, B](base: Schema[B], default: A) = Iso[B, (A, B)](b => (default, b))(_._2)
 
-  def algebra: HAlgebra[SchemaF[Prim, SumTermId, ProductTermId, ?[_], ?], FSchema[
+  type SchemaH[F[_], A] = SchemaF[Prim, SumTermId, ProductTermId, F, A]
+
+  type HSchema[A] = FSchema[
     Prim,
     SumTermId,
     ProductTermId,
-    ?
-  ]]
+    A
+  ]
 
-  def coalgebra: HCoalgebra[SchemaF[Prim, SumTermId, ProductTermId, ?[_], ?], FSchema[
-    Prim,
-    SumTermId,
-    ProductTermId,
-    ?
-  ]]
+  type HEnvSchema[A] = HEnvT[Unit , SchemaH, HSchema, A]
+
+  def algebra: HAlgebra[SchemaH, HEnvSchema] = new (SchemaH[HEnvSchema, ?] ~> HEnvSchema) {
+
+    def apply[A](schema: SchemaH[HSchema, A]): HEnvSchema[A] = schema match {
+      case x: PrimSchemaF[HSchema, a, Prim, SumTermId, ProductTermId]            => HEnvT((), x)
+      case x: SumF[HSchema, a, b, Prim, SumTermId, ProductTermId]                => HEnvT((), x)
+      case x: ProdF[HSchema, a, b, Prim, SumTermId, ProductTermId]               => HEnvT((), x)
+      case x: IsoSchemaF[HSchema, a0, a, Prim, SumTermId, ProductTermId]         => HEnvT((), x)
+      case x: RecordF[HSchema, a0, a, Prim, SumTermId, ProductTermId]            => HEnvT((), x)
+      case x: SeqF[HSchema, a, Prim, SumTermId, ProductTermId]                   => HEnvT((), x)
+      case x: FieldF[HSchema, a, Prim, SumTermId, ProductTermId]                 => HEnvT((), x)
+      case x: UnionF[HSchema, a0, a, Prim, SumTermId, ProductTermId]             => HEnvT((), x)
+      case x: BranchF[HSchema, a, Prim, SumTermId, ProductTermId]                => HEnvT((), x)
+      case x: One[HSchema, Prim, SumTermId, ProductTermId]                       => HEnvT((), x)
+      case x: SelfReference[HSchema, HSchema, a, Prim, SumTermId, ProductTermId] => HEnvT((), x)
+    }
+  }
+
+  def coalgebra: HCoalgebra[SchemaH, HSchema] = new (HSchema ~> SchemaH[HSchema, ?]) {
+    def apply[A](schema: HSchema[A]): SchemaH[HSchema, A] = ???
+  }
 }
 
 final case class AddField[A, Prim[_], SumTermId, ProductTermId](
@@ -40,8 +58,7 @@ final case class AddField[A, Prim[_], SumTermId, ProductTermId](
 trait HasMigrations[R <: Realisation] extends SchemaModule[R] {
 
   import Scalaz._
-
-
+  import R._
 
   final class UpgradingSchema(step: MigrationStep[R.Prim, R.SumTermId, R.ProductTermId]) {
     type Err = String
