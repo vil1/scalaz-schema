2fe8cb95778d6d63a5c59808fb619ea5fec670a7
Graf_Blutwurst <dominicegger@bluewin.ch>
2019-03-21 14:45:55 +0100

    cleanup

--- modules/core/src/main/scala/migrations.scala
+++ modules/core/src/main/scala/migrations.scala
@@ -8,10 +8,7 @@ import SchemaF._
 
 import monocle.Iso
 
-sealed trait MigrationStep[Prim[_], SumTermId, ProductTermId] {
-
-  //private def addField[A, B](base: Schema[B], default: A) = Iso[B, (A, B)](b => (default, b))(_._2)
-
+sealed trait MigrationStep[Prim[_], SumTermId, ProductTermId, G[_[_], _]] {
   type SchemaH[F[_], A] = SchemaF[Prim, SumTermId, ProductTermId, F, A]
 
   type HSchema[A] = FSchema[
@@ -21,27 +18,34 @@ sealed trait MigrationStep[Prim[_], SumTermId, ProductTermId] {
     A
   ]
 
-  type HEnvSchema[F[_], A] = HEnvTK[Option, SchemaH, F, A]
 
-  def algebra: HAlgebra[HEnvSchema, HSchema] = new (HEnvSchema[HSchema, ?] ~> HSchema) {
 
-    def apply[A](env: HEnvSchema[HSchema, A]): HSchema[A] =
-      env.ask.fold[HSchema[A]](Fix(env.fa))(a => Fix(ConstSchemaF(a)))
-  }
+  //private def addField[A, B](base: Schema[B], default: A) = Iso[B, (A, B)](b => (default, b))(_._2)
 
-  def coalgebra: HCoalgebra[HEnvSchema, HSchema] = new (HSchema ~> HEnvSchema[HSchema, ?]) {
-    def apply[A](schema: HSchema[A]): HEnvSchema[HSchema, A] = ???
-  }
+
+
+  def algebra: HAlgebra[G, HSchema] 
+
+  def coalgebra: HCoalgebra[G, HSchema]
+}
+
+object MigrationStep{
+
 }
 
 final case class AddField[A, Prim[_], SumTermId, ProductTermId](
   name: ProductTermId,
   schema: FSchema[Prim, SumTermId, ProductTermId, A],
   default: A
-) extends MigrationStep[Prim, SumTermId, ProductTermId] {
+) extends MigrationStep[Prim, SumTermId, ProductTermId, HEnvTK[Option , SchemaF[Prim, SumTermId, ProductTermId, ?[_], ?], ?[_], ?]] {
+  type HEnvSchema[F[_], A] = HEnvTK[Option , SchemaH, F, A]
 
-  override val algebra   = ???
-  override val coalgebra = ???
+  override val algebra   = new (HEnvSchema[HSchema, ?] ~> HSchema) {
+    def apply[A](env: HEnvSchema[HSchema, A]): HSchema[A] = env.ask.fold[HSchema[A]](Fix(env.fa))(a => Fix(ConstSchemaF(a)))
+  }
+  override val coalgebra  = new (HSchema ~> HEnvSchema[HSchema, ?]) {
+    def apply[A](schema: HSchema[A]): HEnvSchema[HSchema, A] = ???
+  }
 }
 
 trait HasMigrations[R <: Realisation] extends SchemaModule[R] {
@@ -49,7 +53,7 @@ trait HasMigrations[R <: Realisation] extends SchemaModule[R] {
   import Scalaz._
   import R._
 
-  final class UpgradingSchema(step: MigrationStep[R.Prim, R.SumTermId, R.ProductTermId]) {
+  final class UpgradingSchema[G[_[_], _]:HFunctor](step: MigrationStep[R.Prim, R.SumTermId, R.ProductTermId,G]) {
     type Err = String
 
     def to[A](current: Schema[A]): Schema[A] = hyloNT(step.coalgebra, step.algebra).apply(current)
@@ -57,7 +61,7 @@ trait HasMigrations[R <: Realisation] extends SchemaModule[R] {
 
   object Schema {
 
-    def upgradingVia(step: MigrationStep[R.Prim, R.SumTermId, R.ProductTermId]): UpgradingSchema =
+    def upgradingVia[G[_[_], _]:HFunctor](step: MigrationStep[R.Prim, R.SumTermId, R.ProductTermId,G]): UpgradingSchema[G] =
       new UpgradingSchema(step)
   }
 
