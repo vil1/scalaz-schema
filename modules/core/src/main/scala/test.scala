bc7d11e5eb0f6a0df4c65a9042ff0a49bfb6c004
Valentin Kasas <valentin.kasas@gmail.com>
2019-03-24 16:46:57 +0100

    Temporarily add a test file

---
+++ modules/core/src/main/scala/test.scala
@@ -0,0 +1,75 @@
+package scalaz
+
+package schema
+
+import monocle.Iso
+
+final case class Person(name: String, role: Option[Role])
+sealed trait Role
+final case class User(active: Boolean, boss: Person) extends Role
+final case class Admin(rights: List[String])         extends Role
+
+object Person {
+
+  private def f(p: Person): (Seq[Char], Option[Role]) = (p.name.toSeq, p.role)
+  private def g(t: (Seq[Char], Option[Role])): Person = Person(t._1.mkString, t._2)
+
+  val personToTupleIso = Iso[Person, (Seq[Char], Option[Role])](f)(g)
+}
+
+trait TestModule extends SchemaModule[JsonSchema.type] with HasMigrations[JsonSchema.type] {
+  val R = JsonSchema
+
+  type PersonTuple = (Seq[Char], Option[Role])
+
+  val user = record(
+    "active" -*>: prim(JsonSchema.JsonBool) :*: "boss" -*>: self[Rec, Person](person),
+    Iso[(Boolean, Person), User]((User.apply _).tupled)(u => (u.active, u.boss))
+  )
+
+  val admin = record(
+    "rights" -*>: seq(prim(JsonSchema.JsonString)),
+    Iso[List[String], Admin](Admin.apply)(_.rights)
+  )
+
+  val role = union(
+    "user" -+>: user :+:
+      "admin" -+>: admin,
+    Iso[User \/ Admin, Role] {
+      case -\/(u) => u
+      case \/-(a) => a
+    } {
+      case u @ User(_, _) => -\/(u)
+      case a @ Admin(_)   => \/-(a)
+    }
+  )
+
+  def person = record(
+    "name" -*>: prim(JsonSchema.JsonString) :*:
+      "role" -*>: optional(
+      role
+    ),
+    Iso[(String, Option[Role]), Person]((Person.apply _).tupled)(p => (p.name, p.role))
+  )
+
+  val personTupleSchema = iso[Person, PersonTuple](
+    person,
+    Person.personToTupleIso
+  )
+
+  val personV1 =
+    Schema
+      .upgradingVia(AddField("name", "John Doe"))
+      .to(person)
+
+  /*val personV1_ =
+    iso(
+      iso(
+        "role" -> optional(role),
+        Iso[Option[Role], (String, Option[Role])](opt => ("John Doe", opt))(_._2)
+      ),
+      Iso[(String, Option[Role]), Person]((Person.apply _).tupled)(p => (p.name, p.role))
+    )*/
+
+//  val Upgrading(personV1).via(AddField("name", prim(JsonSchema.JsonString), 0)) = person
+}
